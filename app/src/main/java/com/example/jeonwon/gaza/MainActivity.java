4ae70909d3f33b6f67c6549137f27c74cd3f34aa
HeeJack <ljw6828@gmail.com>
2018-03-08 00:37:56 +0900

    18/03/08
    Make a Project

---
+++ app/src/main/java/com/example/jeonwon/gaza/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jeonwon.gaza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
