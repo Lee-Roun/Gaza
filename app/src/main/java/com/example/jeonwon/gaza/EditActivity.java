7216301fde3348d4da243587cb73cd69940c6ebe
HeeJack <ljw6828@gmail.com>
2018-03-13 16:56:10 +0900

    18/03/13 17:00
    뷰페이저, 탭리스너, 뷰리스너 다는 중

--- app/src/main/java/com/example/jeonwon/gaza/EditActivity.java
+++ app/src/main/java/com/example/jeonwon/gaza/EditActivity.java
@@ -1,16 +1,51 @@
 package com.example.jeonwon.gaza;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TableLayout;
+
 
 /**
  * Created by JeonWon on 2018-03-10.
  */
 
 public class EditActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit);
+        setContentView(R.layout.tapmain);
 
+        tabLayout = (TabLayout)findViewById(R.id.tabHost);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+
+        TabPagerAdapter pagerAdapter = new TabPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/jeonwon/gaza/TabPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.jeonwon.gaza;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by JeonWon on 2018-03-13.
+ */
+
+public class TabPagerAdapter extends FragmentStatePagerAdapter {
+
+    private int tabCount;
+
+    public TabPagerAdapter(FragmentManager fm, int tabCount) {
+        super(fm);
+        this.tabCount = tabCount;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position){
+            case 0:
+                FirstFragment FirstFragment = new FirstFragment();
+                return FirstFragment;
+            case 1:
+                SecondFragment SecondFragment = new SecondFragment();
+                return SecondFragment;
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
